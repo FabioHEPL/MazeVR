91dbce1ca9574febf428b2b72b9c95ca27c4014f
Thomas <Capitain.thomas@gmail.com>
2019-08-20 16:10:22 +0200

    Partie Thomas (rotate, drop)

---
+++ Assets/Thomas/Script/EnnemiDrop.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiDrop : MonoBehaviour
+{
+    public LayerMask ennemiSpawn;
+
+    public GameObject ennemi;
+
+    public GameObject ennemiPositionOnMouse;
+
+    [Header("Debug")]
+    public Vector3 positionMouse;
+    public float amplificator = 3;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit rayHit;
+
+            if (ennemiPositionOnMouse == null)
+            {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, ennemiSpawn))
+                {
+                    ennemiPositionOnMouse = Instantiate(ennemi, transform.position = rayHit.collider.transform.position, Quaternion.identity);
+                }
+            }
+            positionMouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            MousePosition(positionMouse);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            RaycastHit rayHit;
+
+            if (ennemiPositionOnMouse == null)
+            {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, ennemiSpawn))
+                {
+                    ennemiPositionOnMouse.transform.position = rayHit.collider.transform.position;
+                }
+            }
+            ennemiPositionOnMouse = null;
+        }
+    }
+
+    private void MousePosition(Vector3 positionMouse)
+    {
+        Vector3 mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        if (positionMouse != mousePosition)
+        {
+            Debug.Log("bouge");
+            ennemiPositionOnMouse.transform.position = ennemiPositionOnMouse.transform.position.normalized + (new Vector3(mousePosition.x, 0, mousePosition.y));
+        }
+    }
+}
---
+++ Assets/Thomas/Script/SelectionObject.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionObject : MonoBehaviour
+{
+    [SerializeField] private LayerMask selectableObject;
+
+    float _rotate = 90;
+    bool boutonRotateRightActiv = false;
+    bool boutonRotateLeftActiv = false;
+
+    public GameObject objectSelected1;
+    public GameObject objectSelected2;
+
+    public Material hightlight;
+    public Material baseMaterial;
+
+    public KeyCode rotateR;
+    public KeyCode rotateL;
+    public KeyCode swap;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, selectableObject))
+            {
+                if (objectSelected1 == null)
+                {
+                    objectSelected1 = rayHit.collider.gameObject;
+                    objectSelected1.GetComponent<Renderer>().material = hightlight;
+                }
+                else if (objectSelected1 != null)
+                {
+                    objectSelected2 = rayHit.collider.gameObject;
+                    objectSelected2.GetComponent<Renderer>().material = hightlight;
+
+                    if (objectSelected2 == objectSelected1)
+                    {
+                        objectSelected2 = null;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(rotateR) && objectSelected1 != null)
+        {
+            boutonRotateRightActiv = true;
+            RotateWall(objectSelected1, boutonRotateRightActiv, boutonRotateLeftActiv);
+            objectSelected1 = null;
+        }
+
+        if (Input.GetKeyDown(rotateL) && objectSelected1 != null)
+        {
+            boutonRotateLeftActiv = true;
+            RotateWall(objectSelected1, boutonRotateRightActiv, boutonRotateLeftActiv);
+            objectSelected1 = null;
+        }
+
+        if (Input.GetKeyDown(swap) && objectSelected1 != null && objectSelected2 != null)
+        {
+            SwitchObject(objectSelected1, objectSelected2);
+            objectSelected1 = null;
+            objectSelected2 = null;
+        }
+    }
+
+    private void SwitchObject(GameObject s1, GameObject s2)
+    {
+        Vector3 switcherObject = s1.transform.position;
+
+        s1.transform.position = s2.transform.position;
+        s2.transform.position = switcherObject;
+
+        s1.GetComponent<Renderer>().material = baseMaterial;
+        s2.GetComponent<Renderer>().material = baseMaterial;
+    }
+
+    void RotateWall(GameObject o, bool bRA, bool bLA)
+    {
+        if (bRA == true)
+        {
+            o.transform.Rotate(0.0f, _rotate, 0.0f);
+            o.GetComponent<Renderer>().material = baseMaterial;
+            bRA = false;
+        }
+        else if (bLA == true)
+        {
+            o.transform.Rotate(0.0f, -_rotate, 0.0f);
+            o.GetComponent<Renderer>().material = baseMaterial;
+            bLA = false;
+        }
+
+        if (objectSelected2 != null)
+        {
+            objectSelected2.GetComponent<Renderer>().material = baseMaterial;
+            objectSelected2 = null;
+        }
+    }
+}

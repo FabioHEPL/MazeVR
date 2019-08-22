6d66742be754ac8c6a55aee5f24b64cf1f867c91
hozone <ho3.sharp@gmail.com>
2019-08-22 11:29:06 +0200

    Operations and inputs

---
+++ MazeVR-Server/Assets/Scripts/Etc/Highlight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Highlight : ScriptableObject
+{    
+    [SerializeField]
+    private Material material;
+
+    public Material Material => material;
+
+    private Material initial;
+    private Renderer renderer;
+
+    public void Apply(GameObject gameObject)
+    {
+        renderer = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+        initial = renderer.material;
+        renderer.material = material;
+    }
+
+    public void Undo()
+    {
+        renderer.material = initial;
+    }
+
+    public void Initialise(Material material)
+    {
+        this.material = material;
+    }
+
+}
+
---
+++ MazeVR-Server/Assets/Scripts/Inputs/CustomInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CustomInput : ScriptableObject
+{
+    public KeyCode key;
+    public Operation operation;
+
+    public void Process()
+    {
+        if (Input.GetKeyDown(key))
+            operation.Execute();
+    }
+}

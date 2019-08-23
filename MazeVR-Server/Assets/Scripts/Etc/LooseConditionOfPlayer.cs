71d79b19abe30affe013a3363544f575f0b22c1a
Thomas <Capitain.thomas@gmail.com>
2019-08-23 10:37:59 +0200

    Script déga aux ennemi + win/Loose playerVR

---
+++ MazeVR-Server/Assets/Scripts/Etc/LooseConditionOfPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseConditionOfPlayer : MonoBehaviour
+{
+    [SerializeField] int lifePointCharater = 1;
+    [SerializeField] GameObject playerLose;
+
+    void Update()
+    {
+        if (lifePointCharater == 0)
+        {
+            playerLose.SetActive(true);
+        }
+
+        if (lifePointCharater < 0)
+        {
+            lifePointCharater = 0;
+        }
+    }
+}

1995c5b2963bc45875d807acc566f4306a7c2796
Thomas <Capitain.thomas@gmail.com>
2019-08-27 11:44:01 +0200

    Timer, ennemi spawn, etc.

---
+++ MazeVR-Server/Assets/Scripts/Etc/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public GameObject winCondition;
+
+    public int timeMinute = 15;
+    float timeSecond = 60;
+
+    int timeSecondDisplay;
+
+    void Update()
+    {
+        timeSecond -= Time.deltaTime;
+        timeSecondDisplay = Mathf.RoundToInt(timeSecond);
+
+        if (timeSecondDisplay < 10)
+        {
+            timerText.text = ("0" + timeMinute + " : " + "0" + timeSecondDisplay);
+        }
+        else
+        {
+            timerText.text = (timeMinute + " : " + timeSecondDisplay);
+        }
+
+        if (timeSecond <= 0)
+        {
+            if (timeSecondDisplay != 0)
+            {
+                timeMinute--;
+            }
+
+            timerText.text = (timeMinute + " : " + "00");
+            timeSecond = 60;
+        }
+
+        if (timeMinute < 0 && timeSecondDisplay == 0)
+        {
+            //vérifier la condision de vitoire/defaite et l'afficher
+            if (timeMinute == -1)
+            {
+                timerText.text = ("00 : 00");
+            }
+
+            winCondition.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ MazeVR-Server/Assets/Scripts/Operations/EnnemiSpawn.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject ennemiPrefab;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] LayerMask spwanerLayer;
+    [SerializeField] LayerMask toAvoidLayer;
+
+    [SerializeField] bool drag = false;
+
+    public Collider[] youShouldNotPass;
+    GameObject ennemiToDragAndDroping;
+    public Vector3 startDroppingPosition;
+
+    void Update()
+    {
+        // inputs
+        if (Input.GetMouseButtonDown(1))
+        {       
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, spwanerLayer))
+            {
+
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, spwanerLayer))
+            {
+
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, enemyLayer))
+            {
+                drag = true;
+                if(ennemiPrefab)
+                ennemiToDragAndDroping = Instantiate(ennemiPrefab, rayHit.point, Quaternion.identity);
+
+                startDroppingPosition = rayHit.point;
+            }
+            
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            drag = false;
+        }
+
+        // drag
+        if (drag == true && ennemiToDragAndDroping)
+        {
+            RaycastHit rayHit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, enemyLayer))
+            {
+                youShouldNotPass = Physics.OverlapSphere(rayHit.point, 1f, toAvoidLayer);
+
+                if (youShouldNotPass.Length > 0)
+                {
+                    drag = false;
+                }
+                else
+                {
+                    ennemiToDragAndDroping.transform.position = rayHit.point;
+                }
+
+                if (Input.GetMouseButton(1))
+                {
+                    bool isTouchingWall = youShouldNotPass.Length > 0;
+                    if (isTouchingWall)
+                    {
+                        ennemiToDragAndDroping.transform.position = startDroppingPosition;
+                        drag = true;
+                        return;
+                    }
+                    else
+                    {
+                    ennemiToDragAndDroping.transform.position = rayHit.point;
+                    drag = true;
+                    return;
+                    }
+                }
+            }
+        }
+    }
+}

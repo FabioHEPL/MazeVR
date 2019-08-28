ec9f1c5ffeaf351ba6a6115ebab1bd7e5a96ec90
hozone <ho3.sharp@gmail.com>
2019-08-28 17:18:04 +0200

    Intégration pour prototype

---
+++ MazeVR-Client/Assets/Eloi/RotateOnMySelf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnMySelf : MonoBehaviour
+{
+    
+    public void SetRotationWith(int rotationType)
+    {
+        transform.localRotation *= Quaternion.Euler(0, 90f * rotationType, 0);
+       
+    }
+}

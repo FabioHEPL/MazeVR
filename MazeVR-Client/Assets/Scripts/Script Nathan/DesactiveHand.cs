13f013c6deefdf1c9690f4370f7288d544d99675
ArchimusW <duquesne.stevens@gmail.com>
2019-08-28 14:31:45 +0200

    Outil VRTK desactivers mains

---
+++ MazeVR-Client/Assets/Scripts/Script Nathan/DesactiveHand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactiveHand : MonoBehaviour
+{
+    public LayerMask layer;
+    public GameObject handcollider;
+    public GameObject hand;
+
+
+    private void OnTriggerEnter(Collider layer)
+    {
+        if (handcollider)
+        {
+            hand.SetActive(false);
+            Debug.Log("Bye Bye !");
+        }
+    }
+
+    private void OnTriggerExit(Collider layer)
+    {
+        if (handcollider)
+        {
+            hand.SetActive(true);
+            Debug.Log("Hi Again!");
+        }
+    }
+}

539c40f09dfd804347c42bcfa338b892d49a77ef
hozone <ho3.sharp@gmail.com>
2019-08-29 11:41:53 +0200

    Curved UI integration

---
+++ MazeVR-Server/Assets/Scripts/Etc/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MazeVR.Server
+{
+    public interface Player
+    {
+        event EventHandler<DeathArgs> Death;
+    }
+
+    public class DeathArgs : EventArgs
+    {
+
+    }
+
+    public interface ITimer
+    {
+        event EventHandler<TimeOutArgs> TimeOut;
+
+    }
+
+    public class TimeOutArgs : EventArgs
+    {
+    }
+}
---
+++ MazeVR-Server/Assets/Scripts/Etc/LoseCondition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MazeVR.Server
+{
+    public class LoseCondition : MonoBehaviour
+    {
+        [SerializeField]
+        private Player player;
+
+        [SerializeField]
+        private ITimer timer;
+
+        // OOD:
+        // Should be subscribed to Fulfilled event
+        [SerializeField]
+        private int LosePanel;
+
+        private void OnEnable()
+        {
+            this.player.Death += Player_Death;
+            this.timer.TimeOut += Timer_TimeOut;
+        }
+
+        private void OnDisable()
+        {
+            this.player.Death -= Player_Death;
+            this.timer.TimeOut -= Timer_TimeOut;
+        }
+
+        private void Player_Death(object sender, DeathArgs args)
+        {
+            
+        }
+
+        private void Timer_TimeOut(object sender, TimeOutArgs args)
+        {
+            
+        }
+
+        // OOD:
+        // Should be a event, and other entities would do something about it
+        public void Fulfill()
+        {
+            // display lose panel
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

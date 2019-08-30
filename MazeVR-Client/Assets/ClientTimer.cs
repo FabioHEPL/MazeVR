d920e6cdbf76940f4dd9dbd69124b1b12fa08a73
hozone <ho3.sharp@gmail.com>
2019-08-30 10:54:41 +0200

    Timer

---
+++ MazeVR-Client/Assets/ClientTimer.cs
@@ -0,0 +1,74 @@
+﻿using MazeVR.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MazeVR.Client
+{
+    public class ClientTimer : NetworkBehaviour
+    {
+        [SerializeField]
+        private float seconds = 10;
+        public float Seconds
+        {
+            get { return seconds; }
+            set { seconds = value; }
+        }
+
+        public override void Synchronize(OscMessage message)
+        {
+            switch (message.address)
+            {
+                case "Began":
+                    OnBegan(message);
+                    break;
+            }
+        }
+
+        public event Action Began;
+
+        public Action OnTick { get; set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void OnBegan(OscMessage message)
+        {
+            this.seconds = message.GetFloat(1);
+            this.Begin();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Begin()
+        {
+            StartCoroutine(StartTimer());
+            this.Began?.Invoke();
+        }
+
+        protected IEnumerator StartTimer()
+        {
+            float currentTime = 0;
+            while (currentTime < seconds)
+            {
+                currentTime += Time.deltaTime;
+                OnTick();
+                yield return null;
+            }
+        }
+
+        protected void OnEnded()
+        {
+
+        }
+    }
+
+}
---
+++ MazeVR-Server/Assets/ServerTimer.cs
@@ -0,0 +1,74 @@
+﻿using MazeVR.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MazeVR.Server
+{
+    public class ServerTimer : NetworkBehaviour
+    {
+        [SerializeField]
+        private float totalSeconds;
+        public float TotalSeconds
+        {
+            get { return totalSeconds; }
+            set { totalSeconds = value; }
+        }
+
+        public Action OnStart { get; set; }
+        public Action OnEnd { get; set; }
+        public Action<float> OnTick { get; set; }        
+
+        public void Begin()
+        {
+            OnBegan();
+        }
+
+        private void OnBegan()
+        {
+
+
+            OnUpdated(new NetworkBehaviourUpdatedArgs("Began", totalSeconds));
+        }
+
+        //private void OnTick()
+        //{
+
+        //}
+
+        public override void Synchronize(OscMessage message)
+        {
+            
+        }
+
+        //public void Begin()
+        //{
+        //    StartCoroutine(StartTimer());
+        //    this.Began?.Invoke();
+        //}
+
+        protected IEnumerator StartTimer()
+        {
+            float currentTime = 0;
+            while (currentTime < totalSeconds)
+            {
+                currentTime += Time.deltaTime;
+                //OnTick();
+                yield return null;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

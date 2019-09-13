9a7e2f92951b5ac9c74656b6683ae7956744f849
JGPaquay <furnhout@gmail.com>
2019-09-13 17:29:33 +0200

    ndef text data reader

---
+++ Cow's Robber/Assets/Scripts/RayCast.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    private RayCast myhit = new RayCast();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        myhit = Camera.main.ScreenPointToRay(input.);
+
+    }
+}
